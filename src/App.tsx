ea2a20d5f14f1208b0de389c2399a50c8b1ab9ea
Rajesh Sivanesan <rajeshsivacse@gmail.com>
2023-09-23 13:10:47 +0530

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -28,12 +28,6 @@ function App() {
   return (
     <Provider store={store}>
       <SnackbarProvider>
-        <div className={styles.container}>
-          <a href="https://trading.bitfinex.com/t?demo=true" target="_blank">
-            <img src={orderBookLogo} className="logo orderbook" alt="Order Book logo" />
-          </a>
-          <h3>Bitfinex Order Book</h3>
-        </div>
         <Header />
         <OrderBook windowWidth={windowWidth} isFeedKilled={isFeedKilled} />
         <Footer killFeedCallback={toggleFeed} isFeedKilled={isFeedKilled} />
