7b5bed74096937ed2fa730af5c6bc8a0369d5c70
Rajesh Sivanesan <rajeshsivacse@gmail.com>
2023-09-24 14:31:34 +0530

    fixed github pages build

--- src/components/OrderBook/index.tsx
+++ src/components/OrderBook/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent } from 'react';
 import { useSelector } from "react-redux";
 import { useBitfinexHook } from '../../hooks/useBitfinexHook';
 import styles from './styles.module.scss';
